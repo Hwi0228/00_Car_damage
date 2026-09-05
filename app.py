cb36d82720c2ca06c7b8013f00056817e1ff11b2
Hwi0228 <your-email@example.com>
2026-09-05 11:46:01 +0900

    feat: UI 추가, demo 제작

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', damage_result=None)
+
+@app.route('/predict/damage', methods=['POST'])
+def predict_damage():
+    file = request.files.get('file')
+    if file:
+        result_data = { ##### 여기에 나중에 api 돌려서 나온 거 출력
+            'output_image': 'https://placehold.co/600x400?text=Test%20Image',
+            'part': '운전석 도어',
+            'type': '문콕 / 스크래치',
+            'repair': 'PDR (덴트 복원) 및 붓펜',
+            'cost': '약 50,000원 ~ 100,000원'
+        }
+        return render_template('index.html', damage_result=result_data)
+    
+    return render_template('index.html', damage_result=None)
+
+@app.route('/predict/tracking', methods=['POST'])
+def predict_tracking():
+    # 추적 기능 추론 로직
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
